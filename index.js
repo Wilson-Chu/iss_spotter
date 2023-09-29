d19995bf864f05dd95208213b0abdf8ecc7a7f9e
Wilson Chu <wilson1.chu@gmail.com>
2023-09-28 23:15:19 -0400

    implemented fetchCoordsByIP function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -7,4 +7,13 @@ const { fetchMyIP } = require('./iss');
 //   }
 
 //   console.log('It worked! Returned IP:', ip);
+// });
+
+// fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coordinates:', coordinates);
 // });
